b527e39dd093a18c75db6a32acb89385726b3bda
yunus emre fersiz <yemref0716@hotmail.com>
2024-07-04 02:32:42 +0300

    feat: Add essential libraries : tailwind, nuxt-image, i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  compatibilityDate: "2024-07-04",
+  css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/i18n", "@nuxt/image","vue-sweetalert2/nuxt"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
